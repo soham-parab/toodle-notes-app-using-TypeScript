30b74284396e533e1cabd8fa6465979cbba04eff
soham-parab <parabsoham@gmail.com>
2021-06-12 01:30:11 +0530

    integrated auth0 authentication

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,11 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Home } from "./Components/Home/Home";
 import { Navbar } from "./Components/Navbar/Navbar";
-
+import { Login } from "./Components/Login/Login";
+import { Logout } from "./Components/Logout/Logout";
 function App() {
    return (
       <div className="App">
          <Navbar />
+         <Login />
+         <Logout />
          <Routes>
             <Route path="/" element={<Home />} />
          </Routes>
---
+++ my-app/src/Components/LandingPage/LandingPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LandingPage = () => {
+   return <div>djwakdojkad</div>;
+};
+
+export default LandingPage;
